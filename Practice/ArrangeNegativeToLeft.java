26ed71721c147f678257352dbd0022bc9a483647
Siddharth Sharma <siddharthsharma@Siddharths-MacBook-Air.local>
2024-11-21 22:15:43 +0530

    practice

---
+++ Practice/ArrangeNegativeToLeft.java
@@ -0,0 +1,24 @@
+package Practice;
+
+import java.util.Arrays;
+
+public class ArrangeNegativeToLeft {
+    //    Input: -12, 11, -13, -5, 6, -7, 5, -3, -6
+    //    Output: -12 -13 -5 -7 -3 -6 11 6 5
+    public static void main(String[] args) {
+        int[] arr = new int[]{-12, 11, -13, -5, 6, -7, 5, -3, -6};
+
+        int j = 0, temp;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < 0) {
+                if (i != j) {
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+                j++;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
